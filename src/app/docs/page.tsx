466f6b72ceb73171f360d152f1f8931a23de9385
Chang Hyeon Kim <53136613+kchsoft@users.noreply.github.com>
2026-08-28 17:50:09 +0900

    feat: expose Swagger documentation page

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+import "swagger-ui-react/swagger-ui.css";
+
+import { ApiDocsPage } from "@/frontend/pages/api-docs/api-docs-page";
+
+export default function DocsPage() {
+  return <ApiDocsPage />;
+}
