e3c2fd868dd4fdace9af60053dac4e76c61ed4a5
Chang Hyeon Kim <53136613+kchsoft@users.noreply.github.com>
2026-09-07 11:33:53 +0900

    refactor(api): pass Board graph settings

--- src/app/api/v1/stories/[storyId]/boards/route.ts
+++ src/app/api/v1/stories/[storyId]/boards/route.ts
@@ -58,6 +58,7 @@ export async function POST(
         name: body.name,
         description: body.description,
         tags: body.tags,
+        graphSettings: body.graphSettings,
       },
       graphDependencies,
     );
